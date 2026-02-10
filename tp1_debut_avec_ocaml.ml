57fc89c121e240846978e619c4e3823da7d62fab
Simon_Mgnz <100791268+simon-mgnz@users.noreply.github.com>
2026-02-10 16:00:10 +0100

    Add files via upload

---
+++ tp1_debut_avec_ocaml.ml
@@ -0,0 +1,33 @@
+(* Ceci est un éditeur pour OCaml
+   Entrez votre programme ici, et envoyez-le au toplevel en utilisant le
+   bouton "Évaluer le code" ci-dessous ou [Ctrl-e]. *)
+
+(* Exercice 1 *)
+let a = log 2.0 in (a+. 8.0)/. (5.0 -. 2.0*. a) ;;
+let b = sqrt 5. in exp b +. 3. *. b /.7.;; 
+
+(* Exercice 2 *)
+3*8;;
+cos 2.;;
+tan 2.0 ;;
+print_newline ();;
+
+(* Exercice 3 *)
+let a = let x = 2 and y = 3 in x + y + 2;;
+let a = let x,y =2,3 in x*y;;
+let a = let x,y = 2, false in  x = 2 || y
+and b = let x,y,z=2,0.0, true in x > int_of_float y && z;;
+
+(* Exercice 4
+Attention aux références ! *)
+let n = 1000 in 
+for k=1 to n-1 do
+  let somme = ref 0 in
+  for l=1 to k/2 do 
+    if k mod l = 0 then somme := !somme + k;
+  done ;
+  if !somme = k then begin
+    print_int(k);
+    print_newline();
+  end
+done;;
