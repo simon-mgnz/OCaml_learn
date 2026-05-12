c8a10721c3956dfac164d08fa09f1ed75b09ae7c
simon-mgnz <100791268+simon-mgnz@users.noreply.github.com>
2026-05-12 16:05:28 +0200

    TP8 part2

--- tp08_les_tas.ml
+++ tp08_les_tas.ml
@@ -26,3 +26,61 @@ Ce n'est pas un tas à cause valeurs qui varient entre grands et petits...
 *)
 
 (* PARTIE 2 : Tamissage et Tassage*)
+
+(* Question 6
+Si le noeud gauche est supérieur à la racine et au noeud droite:
+  Si le noeud droite est supérieur à la racine: 
+    Passer la valeur du noeud gauche à la racine, le noeud droite au noeud gauche et la racine au noeud droite
+  Sinon :
+    Echanger la valeur du noeud gauche avec la racine
+Sinon si le noeud droite est supérieur à la racine:
+  Echanger la valeur du noeud droite à la racine.
+*)
+
+let rec tamiser t i =
+  let parent = t.(i) in
+  if Array.length t < i*3+2 then
+  let fils_gauche, fils_droit = t.((i*3)+1),t.((i*3)+2) in
+  match fils_gauche> parent && fils_gauche > fils_droit with
+  | true when fils_droit>parent -> t.(i) <- fils_gauche ;
+            t.(i*3+1) <- fils_droit;
+            t.(i*3+2) <- parent;
+            tamiser t (i+1)
+  | true -> t.(i) <- fils_gauche;
+            t.(i*3+1) <- parent;
+            tamiser t (i+1)
+  | _ when fils_droit > parent -> t.(i) <- fils_droit;
+                                  t.(i*3+2) <- parent;
+                                  tamiser t (i+1)
+  |_ -> tamiser t (i+1)
+  
+
+let tamiser_correction t i =
+  let n = Array.length t in
+  let rec aux j = match j with
+  | j when 2*j+1 >= n -> () (* feuille *)
+  | k when 2*k+1 = n-1 -> (* un fils gauche *)
+    if t.(j) < t.(2*j+1) then let temp = t.(j) in
+      t.(j) <- t.(2*j+1);
+      t.(2*j+1) <- temp
+  | _-> (* 2 fils *)
+    let m=max t.(2*j+1) t.(2*j+2) in
+    if t.(j)<m then begin
+      let k = if m = t.(2*j+1) then 2*j+1 else 2*j+2 in
+        t.(k)<- t.(j);
+        t.(j) <- m;
+        aux k;
+      end;
+  in aux i;;
+
+(* Complexité O(h) = O(log(n)) (h est la hauteur de l'arbre)*)
+
+(* Question 7 *)
+let tasser t =
+  for k=Array.length t downto 1 do
+    tamiser t k done;;
+(* Complexité : n*O(log(n)) = O(n*log(n))*)
+
+(* PARTIE 3 : Tri par tas *)
+
+(* Question 8 *)
